61eb43db928318d1b54c96a7b2fd72c9ee229b14
juanjose2017 <jjgamboa86@misena.edu.co>
2017-10-13 17:01:09 -0500

    pagina america1 america2, pagina imagenes, oceania

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-clean-css');
  
 gulp.task('server', function() {
   gulp.src('./')
@@ -9,3 +13,19 @@ gulp.task('server', function() {
       open: true
     }));
 });
+
+gulp.task('build-html', function () {
+  return gulp.src(['*.html', 'pa*/*.html'])
+      .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('cs*/*.css', minifyCss()))
+      .pipe(gulp.dest('build'));
+  });
+
+  gulp.task('build-assets-img', function () {
+    return gulp.src('asset*/**/**').
+      pipe(gulp.dest('build'))
+    })
+  
+    
+  gulp.task('build', ['build-html','build-assets-img'])
